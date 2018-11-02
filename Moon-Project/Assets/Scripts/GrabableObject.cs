773d197e7b0cf09f46e910033fa28874f2fc658b
Fletcher Morris <odmoandtheboy@gmail.com>
2018-11-02 05:54:14 +0000

    Minor changes

--- Moon-Project/Assets/Scripts/GrabableObject.cs
+++ Moon-Project/Assets/Scripts/GrabableObject.cs
@@ -8,11 +8,29 @@ public class GrabableObject : MonoBehaviour {
     public string name;
     public Rigidbody body;
     public bool isGrabbed;
+    public bool isHovered;
 
     private void Start()
     {
         if (name == "") name = gameObject.name;
         if (!body) body = GetComponent<Rigidbody>();
         isGrabbed = false;
+        m_renderer = GetComponent<Renderer>();
+        m_originalColour = m_renderer.material.color;
+    }
+
+    private Color m_originalColour;
+    private Renderer m_renderer;
+
+    private void Update()
+    {
+        if(isHovered && !isGrabbed)
+        {
+            m_renderer.material.color = Color.blue;
+        }
+        else
+        {
+            m_renderer.material.color = m_originalColour;
+        }
     }
 }
