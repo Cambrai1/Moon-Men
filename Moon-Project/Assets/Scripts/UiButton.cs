d3b0ceaa546b20e06d82127739d9a57836ec19cd
Fletcher Morris <odmoandtheboy@gmail.com>
2018-11-08 11:13:47 +0000

    Minor changes

---
+++ Moon-Project/Assets/Scripts/UiButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class UiButton : MonoBehaviour {
+
+    private BoxCollider m_col;
+    private RectTransform m_rect;
+
+    private void Start()
+    {
+        m_rect = GetComponent<RectTransform>();
+
+        m_col = GetComponent<BoxCollider>();
+        if(m_col == null)
+        {
+            m_col = gameObject.AddComponent<BoxCollider>();
+        }
+        m_col.size = m_rect.sizeDelta;
+    }
+}
