199012ec9eb9dd537995d8f51324edc93ebd176b
Fletcher Morris <odmoandtheboy@gmail.com>
2018-11-09 23:01:19 +0000

    Grabbable objects now detect hand presence
    Objects can now detect hand position using either basic distance criteria, or by checking for the hand position withing the collider bounds.

--- Moon-Project/Assets/Scripts/GrabableObject.cs
+++ Moon-Project/Assets/Scripts/GrabableObject.cs
@@ -18,15 +18,23 @@ public class GrabableObject : MonoBehaviour {
     private Collider m_col;
     [SerializeField]
     private float grabRange = 0.2f;
+    [SerializeField]
+    private float m_checkRange = 0.5f;
     private Color m_originalColour;
     private Renderer m_renderer;
+    private Transform m_transform;
 
     [SerializeField]
     private PlayerController m_player;
-    private Transform m_playerRightHand, m_playerLeftHand;
+    private Transform m_rHand, m_lHand;
+    private Transform m_closestHand;
+    [SerializeField]
+    private float m_handDistance;
 
     private void Start()
     {
+        m_transform = gameObject.transform;
+
         if (name == "") name = gameObject.name;
         if (!body) body = GetComponent<Rigidbody>();
 
@@ -47,11 +55,10 @@ public class GrabableObject : MonoBehaviour {
 
         if (m_player)
         {
-            m_playerRightHand = m_player.rHandTransform;
-            m_playerLeftHand = m_player.lHandTransform;
+            m_rHand = m_player.rHandTransform;
+            m_lHand = m_player.lHandTransform;
         }
         else return;
-
 
         m_ready = true;
     }
@@ -59,6 +66,24 @@ public class GrabableObject : MonoBehaviour {
     private void Update()
     {
         if (!m_ready) return;
+
+        GetHandDistances();
+
+        isHovered = false;
+        if (!useCollider)
+        {
+            if (m_handDistance <= grabRange) isHovered = true;
+        }
+        else
+        {
+            if(m_handDistance <= m_checkRange)
+            {
+                if (m_col.bounds.Contains(m_closestHand.position))
+                {
+                    isHovered = true;
+                }
+            }
+        }
 
         if(isHovered && !isGrabbed)
         {
@@ -68,5 +93,13 @@ public class GrabableObject : MonoBehaviour {
         {
             m_renderer.material.color = m_originalColour;
         }
+    }
+
+    public void GetHandDistances()
+    {
+        float rHandDist = Vector3.Distance(m_transform.position, m_rHand.position);
+        float lHandDist = Vector3.Distance(m_transform.position, m_lHand.position);
+        if (rHandDist <= lHandDist) { m_closestHand = m_rHand; m_handDistance = rHandDist; }
+        else { m_closestHand = m_lHand; m_handDistance = lHandDist; }
     }
 }
