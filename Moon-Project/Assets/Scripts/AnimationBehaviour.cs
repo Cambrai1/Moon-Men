8db75dfd8ed2e065177bbd54ff47c344e50d8529
samuel.jones@students.plymouth.ac.uk <10Cambrai!>
2018-11-15 10:37:13 +0000

    looking into changing animation to LERP movement

---
+++ Moon-Project/Assets/Scripts/AnimationBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBehaviour : MonoBehaviour {
+
+    Animator m_Animator;
+    float state = 0;
+	// Use this for initialization
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.X) && (state == 0))
+        {
+                m_Animator.Play("Door_Open");
+                state = 1;           
+        }
+        if (Input.GetKey(KeyCode.Z) && (state == 1))
+        {
+                m_Animator.Play("Door_Close");
+                state = 0;
+        }
+	}
+}
