2d25344be7258f3941248f6b5dbde92781b502f2
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2018-11-18 07:28:22 -0700

    Created Inner and Outer Door Animations with simple press controller

--- Moon-Project/Assets/Scripts/AnimationBehaviour.cs
+++ Moon-Project/Assets/Scripts/AnimationBehaviour.cs
@@ -6,12 +6,12 @@ public class AnimationBehaviour : MonoBehaviour {
 
     Animator m_Animator;
     float state = 0;
-	// Use this for initialization
+
 	void Start () {
         m_Animator = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
         if (Input.GetKey(KeyCode.X) && (state == 0))
         {
@@ -24,4 +24,5 @@ public class AnimationBehaviour : MonoBehaviour {
                 state = 0;
         }
 	}
+
 }
