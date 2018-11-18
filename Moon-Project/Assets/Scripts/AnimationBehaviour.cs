75da8ce359f6ab1b891955a7a91242922f7a0a08
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2018-11-18 09:34:49 -0700

    Changed Animation Script so it opens doors close to you

--- Moon-Project/Assets/Scripts/AnimationBehaviour.cs
+++ Moon-Project/Assets/Scripts/AnimationBehaviour.cs
@@ -6,23 +6,28 @@ public class AnimationBehaviour : MonoBehaviour {
 
     Animator m_Animator;
     float state = 0;
+    public GameObject Player;
+    public float dist;
 
 	void Start () {
         m_Animator = GetComponent<Animator>();
+        dist = Vector3.Distance(Player.transform.position, this.gameObject.transform.position);
+        print(dist);
 	}
 	
 
 	void Update () {
-        if (Input.GetKey(KeyCode.X) && (state == 0))
+        dist = Vector3.Distance(Player.transform.position, this.gameObject.transform.position);
+        if ((dist <= 5) && (state == 0))
         {
-                m_Animator.Play("Door_Open");
-                state = 1;           
+            m_Animator.Play("Door_Open");
+            state = 1;
         }
-        if (Input.GetKey(KeyCode.Z) && (state == 1))
+        else if ((dist > 5) && (state == 1))
         {
-                m_Animator.Play("Door_Close");
-                state = 0;
+            m_Animator.Play("Door_Close");
+            state = 0;
         }
-	}
+    }
 
 }
