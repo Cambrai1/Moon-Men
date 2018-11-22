d8eb7f536b6de90c4701af657c223c13480491b0
Fletcher Morris <odmoandtheboy@gmail.com>
2018-11-22 11:37:18 +0000

    Added a bunch of usefull classes
    Added 'StaticMethods' class, which can contain a bunch of useful methods used throughout the project.
    
    Added 'PlayerDetector' script, which detects the player, and invokes unityevents.
    
    Added DoorController script, for opening doors.

---
+++ Moon-Project/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerDetector : MonoBehaviour {
+
+    [SerializeField]
+    private Transform m_player;
+    private Transform m_transform;
+    [SerializeField]
+    private bool m_useTopDownDistance = true;
+
+    [SerializeField]
+    private float m_triggerDistance = 3.0f;
+    [SerializeField]
+    private float m_untriggerDistance = 4.0f;
+
+    private bool m_triggered = false;
+    private float m_currentDistane;
+
+    public UnityEvent TriggerStart;
+    public UnityEvent TriggerEnd;
+    public UnityEvent TriggerChange;
+
+    private void Start()
+    {
+        m_transform = transform;
+        if (!m_player) m_player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(m_useTopDownDistance) m_currentDistane = Vector2.Distance(StaticMethods.TopDown(m_player.position), StaticMethods.TopDown(m_transform.position));
+        else m_currentDistane = Vector3.Distance(m_player.position, m_transform.position);
+        if (m_currentDistane <= m_triggerDistance && !m_triggered)
+        {
+            m_triggered = true;
+            TriggerStart.Invoke();
+            TriggerChange.Invoke();
+        }
+        else if (m_currentDistane > m_untriggerDistance && m_triggered)
+        {
+            m_triggered = false;
+            TriggerEnd.Invoke();
+            TriggerChange.Invoke();
+        }
+    }
+}
