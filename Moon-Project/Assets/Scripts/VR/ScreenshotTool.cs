fb70eaddb8a354b0c6fff3dfc00141fd2c96606a
Fletcher Morris <odmoandtheboy@gmail.com>
2018-12-04 14:39:38 +0000

    Minor changes

--- Moon-Project/Assets/Scripts/VR/ScreenshotTool.cs
+++ Moon-Project/Assets/Scripts/VR/ScreenshotTool.cs
@@ -19,15 +19,19 @@ public class ScreenshotTool : MonoBehaviour {
     private static void CaptureEditorScreenshot()
     {
         GameObject g = GameObject.Find("ScreenshotCam");
-        Camera c = g.GetComponent<Camera>();
-        ScreenshotTool t = g.GetComponent<ScreenshotTool>();
-        Vector3 pos = SceneView.lastActiveSceneView.camera.transform.position;
-        Quaternion rot = SceneView.lastActiveSceneView.camera.transform.rotation;
-        g.transform.position = pos;
-        g.transform.rotation = rot;
-        c.enabled = true;
-        t.Capture(c, 1);
-        c.enabled = false;
+        if(g != null)
+        {
+            Camera c = g.GetComponent<Camera>();
+            ScreenshotTool t = g.GetComponent<ScreenshotTool>();
+            Vector3 pos = SceneView.lastActiveSceneView.camera.transform.position;
+            Quaternion rot = SceneView.lastActiveSceneView.camera.transform.rotation;
+            g.transform.position = pos;
+            g.transform.rotation = rot;
+            c.enabled = true;
+            c.fieldOfView = 55.5f;
+            t.Capture(c, 1);
+            c.enabled = false;
+        }
     }
 
 
