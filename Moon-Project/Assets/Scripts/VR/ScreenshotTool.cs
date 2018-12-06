b0cb522ae67a1b0fac7fb235867cbf87ca1b0b9e
Fletcher Morris <odmoandtheboy@gmail.com>
2018-12-06 08:02:21 +0000

    Minor changes

--- Moon-Project/Assets/Scripts/VR/ScreenshotTool.cs
+++ Moon-Project/Assets/Scripts/VR/ScreenshotTool.cs
@@ -15,24 +15,26 @@ public class ScreenshotTool : MonoBehaviour {
 
     private RenderTexture m_tex;
 
-    [MenuItem("Tools/Screenshot")]
-    private static void CaptureEditorScreenshot()
-    {
-        GameObject g = GameObject.Find("ScreenshotCam");
-        if(g != null)
-        {
-            Camera c = g.GetComponent<Camera>();
-            ScreenshotTool t = g.GetComponent<ScreenshotTool>();
-            Vector3 pos = SceneView.lastActiveSceneView.camera.transform.position;
-            Quaternion rot = SceneView.lastActiveSceneView.camera.transform.rotation;
-            g.transform.position = pos;
-            g.transform.rotation = rot;
-            c.enabled = true;
-            c.fieldOfView = 55.5f;
-            t.Capture(c, 1);
-            c.enabled = false;
-        }
-    }
+//#if UNITY_EDITOR
+//    [MenuItem("Tools/Screenshot")]
+//    private static void CaptureEditorScreenshot()
+//    {
+//        GameObject g = GameObject.Find("ScreenshotCam");
+//        if(g != null)
+//        {
+//            Camera c = g.GetComponent<Camera>();
+//            ScreenshotTool t = g.GetComponent<ScreenshotTool>();
+//            Vector3 pos = SceneView.lastActiveSceneView.camera.transform.position;
+//            Quaternion rot = SceneView.lastActiveSceneView.camera.transform.rotation;
+//            g.transform.position = pos;
+//            g.transform.rotation = rot;
+//            c.enabled = true;
+//            c.fieldOfView = 55.5f;
+//            t.Capture(c, 1);
+//            c.enabled = false;
+//        }
+//    }
+//#endif
 
 
     private void Start()
