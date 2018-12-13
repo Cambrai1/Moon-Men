7691189c06cf0bd255c69bdc4b65ef08bd3f1c2e
Fletcher Morris <odmoandtheboy@gmail.com>
2018-12-13 15:54:34 +0000

    Created 'MissionLog' script
    Created a script for managing game missions & tasks.
    Created a ScriptableObject for missions.

---
+++ Moon-Project/Assets/Scripts/MissionLog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionLog : MonoBehaviour
+{
+    public List<Mission> allMissions;
+    public List<Mission> activeMissions;
+    public List<Mission> completedMissions;
+
+    public GameObject missionUiPrefab;
+}
+
+[CreateAssetMenu(fileName = "NewMission", menuName = "Mission", order = 1)]
+public class Mission : ScriptableObject
+{
+    public string title = "New Mission";
+    public string description = "Do a thing";
+}
