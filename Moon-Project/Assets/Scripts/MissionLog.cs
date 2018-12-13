6fd88658bdbf2d9c727c61360a9f43cf04418b55
Fletcher Morris <odmoandtheboy@gmail.com>
2018-12-13 16:27:55 +0000

    Improvements to mission log script

--- Moon-Project/Assets/Scripts/MissionLog.cs
+++ Moon-Project/Assets/Scripts/MissionLog.cs
@@ -5,11 +5,79 @@ using UnityEngine.UI;
 
 public class MissionLog : MonoBehaviour
 {
-    public List<Mission> allMissions;
-    public List<Mission> activeMissions;
-    public List<Mission> completedMissions;
+    [SerializeField]
+    private List<Mission> m_allMissions;                   //  A list of all loaded missions
+    [SerializeField]
+    private List<Mission> m_activeMissions;                //  A list of all active missions
+    [SerializeField]
+    private List<Mission> m_completedMissions;             //  A list of all completed missions
 
-    public GameObject missionUiPrefab;
+    [SerializeField]
+    private GameObject m_missionUiPrefab;               //  The prefab to use when creating new missions
+
+    [SerializeField]
+    private GameObject m_activeMissionsPanel;           //  The panel containing active missions
+    [SerializeField]
+    private GameObject m_completedMissionsPanel;        //  The panel containing completed missions
+    [SerializeField]
+    private Text m_missionDetailsText;                  //  The text detailing the selected mission
+
+
+
+
+    public void ActivateMission(string _missionName)
+    {
+        foreach(Mission m in m_allMissions)
+        {
+            if(m.name == _missionName)
+            {
+                ActivateMission(m);
+                return;
+            }
+        }
+    }
+    public void ActivateMission(Mission _mission)
+    {
+        if(m_activeMissions.Contains(_mission))
+        {
+            Debug.Log("'" + _mission.name + "' already active.");
+        }
+        else
+        {
+            ForceActivateMission(_mission);
+        }
+    }
+    public void ForceActivateMission(Mission mission)
+    {
+        m_activeMissions.Add(mission);
+    }
+
+    public void CompleteMission(string _missionName)
+    {
+        foreach(Mission m in m_activeMissions)
+        {
+            if(m.name == _missionName)
+            {
+                CompleteMission(m);
+                return;
+            }
+        }
+    }
+    public void CompleteMission(Mission _mission)
+    {
+        if(m_completedMissions.Contains(_mission))
+        {
+            Debug.Log("'" + _mission.name + "' already completed.");
+        }
+        else
+        {
+            ForceCompleteMission(_mission);
+        }
+    }
+    public void ForceCompleteMission(Mission _mission)
+    {
+
+    }
 }
 
 [CreateAssetMenu(fileName = "NewMission", menuName = "Mission", order = 1)]
@@ -17,4 +85,5 @@ public class Mission : ScriptableObject
 {
     public string title = "New Mission";
     public string description = "Do a thing";
+    public List<ScriptableObject> unlocksMissions;
 }
