f3dc2f0a45171007ce1772fcaa0ec5827c415892
Fletcher Morris <odmoandtheboy@gmail.com>
2018-12-13 17:06:46 +0000

    Loaded missions can now be displayed on the wrist-ui

--- Moon-Project/Assets/Scripts/MissionLog.cs
+++ Moon-Project/Assets/Scripts/MissionLog.cs
@@ -6,30 +6,37 @@ using UnityEngine.UI;
 public class MissionLog : MonoBehaviour
 {
     [SerializeField]
-    private List<Mission> m_allMissions;                   //  A list of all loaded missions
+    private List<Mission> m_allMissions;            //  A list of all loaded missions
     [SerializeField]
-    private List<Mission> m_activeMissions;                //  A list of all active missions
+    private List<Mission> m_activeMissions;         //  A list of all active missions
     [SerializeField]
-    private List<Mission> m_completedMissions;             //  A list of all completed missions
+    private List<Mission> m_completedMissions;      //  A list of all completed missions
 
     [SerializeField]
-    private GameObject m_missionUiPrefab;               //  The prefab to use when creating new missions
+    private GameObject m_missionUiPrefab;           //  The prefab to use when creating new missions
 
     [SerializeField]
-    private GameObject m_activeMissionsPanel;           //  The panel containing active missions
+    private Transform m_activeMissionsPanel;       //  The panel containing active missions
     [SerializeField]
-    private GameObject m_completedMissionsPanel;        //  The panel containing completed missions
+    private Transform m_completedMissionsPanel;    //  The panel containing completed missions
     [SerializeField]
-    private Text m_missionDetailsText;                  //  The text detailing the selected mission
+    private Text m_missionDetailsText;              //  The text detailing the selected mission
 
 
+    private void Start()
+    {
+        foreach(Mission m in m_allMissions)
+        {
+            if (m.activeByDefault) ActivateMission(m);
+        }
+    }
 
 
     public void ActivateMission(string _missionName)
     {
         foreach(Mission m in m_allMissions)
         {
-            if(m.name == _missionName)
+            if(m.title == _missionName)
             {
                 ActivateMission(m);
                 return;
@@ -40,7 +47,7 @@ public class MissionLog : MonoBehaviour
     {
         if(m_activeMissions.Contains(_mission))
         {
-            Debug.Log("'" + _mission.name + "' already active.");
+            Debug.Log("'" + _mission.title + "' already active.");
         }
         else
         {
@@ -50,13 +57,14 @@ public class MissionLog : MonoBehaviour
     public void ForceActivateMission(Mission mission)
     {
         m_activeMissions.Add(mission);
+        RefreshUi();
     }
 
     public void CompleteMission(string _missionName)
     {
         foreach(Mission m in m_activeMissions)
         {
-            if(m.name == _missionName)
+            if(m.title == _missionName)
             {
                 CompleteMission(m);
                 return;
@@ -67,7 +75,7 @@ public class MissionLog : MonoBehaviour
     {
         if(m_completedMissions.Contains(_mission))
         {
-            Debug.Log("'" + _mission.name + "' already completed.");
+            Debug.Log("'" + _mission.title + "' already completed.");
         }
         else
         {
@@ -76,14 +84,61 @@ public class MissionLog : MonoBehaviour
     }
     public void ForceCompleteMission(Mission _mission)
     {
+        m_completedMissions.Add(_mission);
+        m_activeMissions.Remove(_mission);
+        RefreshUi();
+    }
 
+
+
+    public void RefreshUi()
+    {
+        foreach(Transform t in m_activeMissionsPanel.GetComponentsInChildren<Transform>())
+        {
+            if(t != m_activeMissionsPanel.transform)
+            GameObject.Destroy(t.gameObject);
+        }
+        foreach (Transform t in m_completedMissionsPanel.GetComponentsInChildren<Transform>())
+        {
+            if (t != m_completedMissionsPanel.transform)
+                GameObject.Destroy(t.gameObject);
+        }
+
+        foreach(Mission m in m_activeMissions)
+        {
+            GameObject missionUi = Instantiate(m_missionUiPrefab, m_activeMissionsPanel);
+            missionUi.GetComponentInChildren<Text>().text = m.title;
+        }
+        foreach (Mission m in m_completedMissions)
+        {
+            GameObject missionUi = Instantiate(m_missionUiPrefab, m_completedMissionsPanel);
+            missionUi.GetComponentInChildren<Text>().text = m.title;
+        }
+
+        SelectMission(m_activeMissions[0]);
+    }
+    public void SelectMission(Text _missionText)
+    {
+        foreach (Mission m in m_allMissions)
+        {
+            if (m.title == _missionText.text)
+            {
+                SelectMission(m);
+                return;
+            }
+        }
+    }
+    public void SelectMission(Mission _mission)
+    {
+        m_missionDetailsText.text = _mission.description;
     }
 }
 
 [CreateAssetMenu(fileName = "NewMission", menuName = "Mission", order = 1)]
 public class Mission : ScriptableObject
 {
-    public string title = "New Mission";
+    public string title = "NewMission";
     public string description = "Do a thing";
     public List<ScriptableObject> unlocksMissions;
+    public bool activeByDefault = false;
 }
